a09d03360d0e40046980e653b90cc4b5acbd98ab
HarleyKwyn <kwyn.meagher@gmail.com>
2014-03-22 10:51:05 -0700

    fixes formatting in app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,11 @@
-'use strict';
-(function(){
-
-
-    var app = angular.module('zenbeatApp', []);
-    app.config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+var app = angular.module('zenbeatApp', []);
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+    })
+    .otherwise({
+        redirectTo: '/'
     });
-
-}.call(this));
+});
